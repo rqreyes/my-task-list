30edc6dc8c401f50b11d41914184366384f1f0b3
Randy Reyes <rqreyes@gmail.com>
2026-08-17 14:11:06 -0700

    refactor: add comments

--- database/database.sql
+++ database/database.sql
@@ -1,9 +1,13 @@
+-- create database
+-- ------------------------------------------------------------
 CREATE TABLE tasks (
   id bigint GENERATED ALWAYS AS IDENTITY,
   is_completed BOOLEAN DEFAULT FALSE,
   title VARCHAR(255) NOT NULL
 );
 
+-- add data
+-- ------------------------------------------------------------
 INSERT INTO tasks (is_completed, title) VALUES
 (DEFAULT, 'Hello world'),
 (DEFAULT, 'My first task'),
